d8c51f13518b1aa531bcd5de7e79fb0e43b41b80
Ashwani <aswanibtech@gmail.com>
2019-06-25 08:03:23 +0530

    String method added

---
+++ stringmaniexp.py
@@ -0,0 +1,38 @@
+'''x= input("enter your name")
+name= x
+a= len(name)
+print(a)
+print(name.capitalize())
+print(name.lower())
+print(name.upper())
+print(name.swapcase())
+print(name.title())
+print(name.isdigit())
+print(name.isalpha())
+print(name.islower())
+print(name.isupper())
+print(name.isalnum())
+str = input("enter a string")
+print(str)
+
+find=input("enter the string  u want to find")
+print(find)
+print(str.find('a'))
+rep=input("enter the string  u want to replace")
+print(rep)
+print(str.replace(find,rep))
+
+str1="hello how r u"
+print(str1)
+print(str1.count('h'))
+print(str1.endswith('ufd'))
+print(str1.replace('r','are'))'''
+
+x= input("enter your name")
+print(x)
+print(x.split(','))
+str3="         Ram         "
+print(str3)
+print(str3.lstrip(),"fdfdfd")
+print(str3.rstrip(),"fdfdfd")
+print(str3.strip(),"fdfdfd")
